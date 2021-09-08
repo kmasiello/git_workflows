112e29341a2badb116beb70c9817f872be6a2099
kmasiello <32964481+kmasiello@users.noreply.github.com>
2021-09-08 07:43:44 -0700

    change color

--- sandbox/faithful_wed/app.R
+++ sandbox/faithful_wed/app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data on Wednesday"),
+    titlePanel("Old Faithful Geyser Data on today"),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
@@ -41,7 +41,7 @@ server <- function(input, output) {
         bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
         # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+        hist(x, breaks = bins, col = 'palegreen4', border = 'white')
     })
 }
 
