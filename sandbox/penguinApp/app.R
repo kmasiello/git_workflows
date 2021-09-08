5753b83b4fe240d529b446b7c14a73f8835f9140
kmasiello <32964481+kmasiello@users.noreply.github.com>
2021-09-07 22:11:40 -0700

    stylize penguins

--- sandbox/penguinApp/app.R
+++ sandbox/penguinApp/app.R
@@ -2,12 +2,21 @@ library(shiny)
 library(ggplot2)
 library(dplyr)
 library(palmerpenguins)
+library(bslib)
+library(thematic)
+
+thematic_shiny(font = "auto")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
+    theme  = bslib::bs_theme(
+        bg = "#485862", fg = "#E9EAEE", primary = "#2AA198",
+        # consider #9EC1D8 over #485862
+        base_font = bslib::font_google("Montserrat")),
+
 
     # Application title
-    titlePanel("Penguin Measurements for our Project"),
+    titlePanel("Penguin Measurements"),
 
     sidebarLayout(
         sidebarPanel(radioButtons(inputId = "island",
